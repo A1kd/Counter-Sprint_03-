cb9db157392edb1753a07932c9b976a29fe81cdf
Yamakasi <ignatk38@yandex.ru>
2025-10-20 18:37:55 +0300

    Повышена чистота и корректность кода

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    @IBOutlet weak var counterText: UILabel!
-    let dateFormatter = DateFormatter()
-    var start = 0
+    @IBOutlet private weak var counterText: UILabel!
+    private let dateFormatter = DateFormatter()
+    private var start = 0
 
     
-    @IBOutlet weak var plus: UIButton!
-    @IBAction func plusAction(_ sender: Any) {
+    @IBOutlet private weak var plus: UIButton!
+    @IBAction private func plusAction(_ sender: Any) {
         start += 1
         counterText.text = "Значение счётчика: \(start)"
         history.text += "\n\(dateFormatter.string(from: Date())): значение изменено на +1"
     }
     
     
-    @IBOutlet weak var minus: UIButton!
-    @IBAction func minusAction(_ sender: Any) {
+    @IBOutlet private weak var minus: UIButton!
+    @IBAction private func minusAction(_ sender: Any) {
         if start > 0 {
             start -= 1
             counterText.text = "Значение счётчика: \(start)"
@@ -33,21 +33,20 @@ class ViewController: UIViewController {
             }
     }
     
-    @IBOutlet weak var reset: UIButton!
+    @IBOutlet private weak var reset: UIButton!
     
-    @IBAction func resetAction(_ sender: Any) {
+    @IBAction private func resetAction(_ sender: Any) {
         start = 0
         counterText.text = "Значение счётчика: \(start)"
         history.text += "\n\(dateFormatter.string(from: Date())): значение сброшено"
     }
     
-    @IBOutlet weak var history: UITextView!
+    @IBOutlet private weak var history: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         dateFormatter.dateStyle = .medium
-                dateFormatter.timeStyle = .medium
+        dateFormatter.timeStyle = .medium
         dateFormatter.locale = Locale(identifier: "ru_RU")
         history.text = "История изменений:"
     }
